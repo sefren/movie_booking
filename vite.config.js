e54cc1066fab3a750039b311a02fc0f00a543934
Illegal Soup <illegalsoup@nowhere.local>
2025-11-06 22:31:52 +0530

    use esbuild instead of terser

--- vite.config.js
+++ vite.config.js
@@ -18,13 +18,7 @@ export default defineConfig({
       },
     },
     chunkSizeWarningLimit: 1000,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: 'esbuild', // Use esbuild instead of terser (faster and no extra dependency)
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom'],
